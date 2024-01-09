87bae300c5e61ec0c433a85fa6da8c5f4884ba67
Victor <vlougongs@gmail.com>
2024-01-09 21:19:32 +0000

    Added Logger Middleware!

--- myApp.js
+++ myApp.js
@@ -1,14 +1,18 @@
-require('dotenv').config() 
+require('dotenv').config()
 
 let express = require('express');
 let app = express();
 let indexPath = __dirname + "/views/index.html";
 let publicPath = __dirname + "/public";
-let object = {"message": "Hello json"};
+let object = { "message": "Hello json" };
 
 console.log("Hello World");
 
 app.use("/public", express.static(publicPath));
+app.use("/", function (req, res, next) {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+});
 
 app.get("/", function (req, res) {
     res.sendFile(indexPath);
