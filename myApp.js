ffc300c405115d3e58d9eb53e1a1e6f02201adde
Victor <vlougongs@gmail.com>
2024-01-09 21:20:57 +0000

    Refactored Previous Exercise!

--- myApp.js
+++ myApp.js
@@ -9,7 +9,7 @@ let object = { "message": "Hello json" };
 console.log("Hello World");
 
 app.use("/public", express.static(publicPath));
-app.use("/", function (req, res, next) {
+app.use(function (req, res, next) {
     console.log(req.method + " " + req.path + " - " + req.ip);
     next();
 });
