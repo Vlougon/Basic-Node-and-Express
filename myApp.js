2387f99bfabdce3d612645d318840b132980ddac
Victor <vlougongs@gmail.com>
2024-01-09 19:43:50 +0000

    Added app.use + express.static!

--- myApp.js
+++ myApp.js
@@ -1,11 +1,14 @@
 let express = require('express');
 let app = express();
-let absolutePath = __dirname + "/views/index.html";
+let indexPath = __dirname + "/views/index.html";
+let publicPath = __dirname + "/public";
 
 console.log("Hello World");
 
+app.use("/public", express.static(publicPath));
+
 app.get("/", function (req, res) {
-    res.sendFile(absolutePath);
+    res.sendFile(indexPath);
 });
 
 
