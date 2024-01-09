fb98fac9eed8e441c7086d2db8e63c328aa3fd62
Victor <vlougongs@gmail.com>
2024-01-09 20:27:47 +0000

    Added json ROute + JSON Object Responsense!

--- myApp.js
+++ myApp.js
@@ -2,6 +2,7 @@ let express = require('express');
 let app = express();
 let indexPath = __dirname + "/views/index.html";
 let publicPath = __dirname + "/public";
+let object = {"message": "Hello json"};
 
 console.log("Hello World");
 
@@ -9,6 +10,10 @@ app.use("/public", express.static(publicPath));
 
 app.get("/", function (req, res) {
     res.sendFile(indexPath);
+});
+
+app.get("/json", function (req, res) {
+    res.json(object);
 });
 
 
